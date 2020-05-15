176b6c0dbe4b847a3d5de9c4e9a9918e284ed25d
Shontrell <58537959+Shontrell@users.noreply.github.com>
2020-05-15 11:51:40 -0500

    update main.c
    adding lines

--- main.c
+++ main.c
@@ -8,7 +8,13 @@
 int main(int argc, char **argv)
 {
 	FILE *file = fopen(argv[1], "r");
-
+	char *buffer = NULL;
+	size_t size = 0;
+	unsigned int x = 0;
+	char **token;
+	void (*f)(stack_t **, unsigned int);
+	stack_t *stack = NULL;
+		
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
@@ -19,5 +25,16 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
+	for (; getline(&buffer, &size, file) > 0; x++)
+	{
+		token[0] = strtok(buffer, " ");
+		f = get_op_func(token);
+		token[1] = strtok(NULL, " ");
+		global = atoi(token[1]);
+		f(&stack, x);
+	}
+	free(buffer);
+	free_stack(stack);
+	fclose(file);
 	return (EXIT_SUCCESS);
 }
